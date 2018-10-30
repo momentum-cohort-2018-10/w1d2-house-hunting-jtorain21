051c168da44fbaae65509d49971eb856dfd4c334
joshua torain <jtorain@gmail.com>
2018-10-30 13:14:58 -0400

    house hunting assignment

--- house_hunting.py
+++ house_hunting.py
@@ -1,5 +1,7 @@
 total_cost = float(input("What is the total cost of your dream home?") )
 annual_salary = float(input("What is your annual salary?"))
-portion_saved = float(input("What portion will yoube saving?"))
-portion_down_payment = int(input(0.25))
-current_savings = int(input(0))
+portion_saved = float(input("What portion will you be saving?"))
+portion_down_payment = total_cost * 0.25
+current_savings = 0
+r = current_savings * 0.04/12
+Number of Months = print(total_cost)
